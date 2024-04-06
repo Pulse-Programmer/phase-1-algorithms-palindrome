7da85acf18d0ba8e44cf2297bfb54e24f0d21104
Stephen Ochieng <materiamystic@gmail.com>
2024-04-06 12:19:07 +0300

    Adds palindrome feature

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const charArray = word.split(""); //converts string to array of characters
+
+  for (let i = 0; i < charArray.length; i++) {
+    if (charArray[i] !== charArray[charArray.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
-/* 
-  Add your pseudocode here
+/*
+  Add your pseudocode here:
+  1.split word characters into an array
+  2. Access array elements using for loop
+  3. Check if equal from between first and last character iteratively till last and first character
+  4. return false and exit from function if any mismatch found
+  5. return true if the loop fully runs
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here:
+  The function takes a string, splits that string into an array of its individual characters and then checks
+  for mismatch between last and first characters iteratively from character to next respectively. Any mismatch
+  results to a false return from function as it indicates the word is not a palindrome. The function returns
+  true if all characters match as it skips the first block of if-statement entirely.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +36,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"));
 }
 
 module.exports = isPalindrome;
